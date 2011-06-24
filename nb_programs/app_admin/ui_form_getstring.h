c752139c781c501618fe6ce681b491f927d7dc5d
sealeks@mail.ru <sealeks@mail.ru@7b442b8e-1fa5-4c33-659b-d73a810005db>
2011-06-24 16:26:07 +0000

    2

--- nb_programs/app_admin/ui_form_getstring.h
+++ nb_programs/app_admin/ui_form_getstring.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'form_getstring.ui'
 **
-** Created: Fri May 27 19:50:58 2011
-**      by: Qt User Interface Compiler version 4.6.2
+** Created: Mon 20. Jun 18:25:02 2011
+**      by: Qt User Interface Compiler version 4.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
